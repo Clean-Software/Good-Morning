bb005c7b116583ec9a8bf9226adc2c5799f115e0
Carlos Zoft <carlosk2gunn@hotmail.com>
2022-02-20 13:23:09 -0300

    ✨ feat: add env chore

--- src/express/server.ts
+++ src/express/server.ts
@@ -1,19 +1,20 @@
-import express, { Express } from 'express';
-import setUpRoutes from '../apis'
+import "dotenv/config";
+import express, { Express } from "express";
+import setUpRoutes from "../apis";
 
 export class Server {
-    app :  Express
-    constructor() {
-        this.app = express();
-        this.config();
-        this.routes();
-    }
-    config() {
-        this.app.use(express.json());
-    }
-    routes() {
-        setUpRoutes(this.app);
-    }
-}  
+  app: Express;
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
+  config() {
+    this.app.use(express.json());
+  }
+  routes() {
+    setUpRoutes(this.app);
+  }
+}
 
 export default new Server().app;
