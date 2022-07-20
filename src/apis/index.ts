926f088344b23aabd4a575d999f25a1bd7898df4
CarlosZoft <carlosk2gunn@hotmail.com>
2022-07-19 23:45:35 -0300

    🐛 fix: remove test router

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -2,10 +2,5 @@ import { Express, Request, Response } from "express";
 import { getPhrases } from "./pensador/routes";
 
 export default function setUpRoutes(app: Express) {
-  app.use(
-    "/test",
-    (request: Request, response: Response) =>
-      response.json("Hello, your application is running") // just a test
-  );
   app.use("/getPhrase", getPhrases);
 }
